df779b9e7082b137c2da3b1b1aa563f2d59a106f
Nisrine Jednane-Kane <nisrinekane@nisrines-laptop.lan>
2023-11-10 00:23:11 -0800

    Add logic and forms

---
+++ DownPayment/DPA_app/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+
+class UserInformationForm(forms.Form):
+    first_name = forms.CharField(max_length=250)
+    last_name = forms.CharField(max_length=250)
+    rent_amount = forms.DecimalField(max_digits=10, decimal_places=2)
+    county = forms.CharField(max_length=250)
+    credit_score = forms.IntegerField()
+    liquid_assets = forms.DecimalField(max_digits=10, decimal_places=2)
+    household_income = forms.DecimalField(max_digits=10, decimal_places=2)
+    household_size = forms.IntegerField()
+    is_first_time_buyer = forms.BooleanField(required=False)
+    is_three_year_buyer = forms.BooleanField(required=False)
+    is_female_head_of_household = forms.BooleanField(required=False)
+    is_PEN = forms.BooleanField(required=False)
---
+++ DownPayment/DownPayment/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+from DPA_app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('check_eligibility/', views.check_eligibility, name='check_eligibility'),
+]
