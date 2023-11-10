d19a30e1d15b570f8d0ec09cd6b458f8ecf33f69
Nisrine Jednane-Kane <nisrinekane@nisrines-laptop.lan>
2023-11-10 00:19:35 -0800

    Add logic and forms

--- DownPaymentBackend/DPA_app/populate_db.py
+++ DownPaymentBackend/DPA_app/populate_db.py
@@ -77,5 +77,87 @@ def add_georgia_dream_homebuyers_program():
     georgia_dream_program.save()
     print(f"The program '{program_name}' has been added to the database.")
 
-# Call the function
+
+
+# -----------------------FULTON HOMEOWNERSHIP PROGRAM--------------
+def add_fulton_homeownership_program():
+    program_name = "Fulton Homeownership Program"
+    down_payment_options = {
+        "STANDARD": '10000.00',  # No PEN
+    }
+    
+    income_limits_by_area = {
+        "1": '54000.00',
+        "2": '61700.00',
+        "3": '69400.00',
+        "4": '77100.00',
+        "5": '83300.00',
+        "6": '89450.00',
+        "7": '95650.00',
+        "8": '101800.00',
+    }
+    
+    counties = ["Fulton County"]
+
+    # check for existing program
+    existing_program = Program.objects.filter(name=program_name).first()
+    if existing_program:
+        print(f"The program '{program_name}' already exists in the database.")
+        return
+
+    # create and save new program instance
+    fulton_hop_program = Program(
+        name=program_name,
+        amount=down_payment_options,
+        counties=', '.join(counties),
+        income_limits=income_limits_by_area,
+        liquid_asset_limit='0.00',  # no liquid asset limit specified
+        credit_score_minimum=0  # no credit score minimum specified
+    )
+
+    fulton_hop_program.save()
+    print(f"The program '{program_name}' has been added to the database.")
+
+
+# -----------------------VINE CITY RENAISSANCE INITIATIVE--------------
+def add_vine_city_renaissance_initiative():
+    program_name = "Vine City Renaissance Initiative"
+    down_payment_options = {
+        "STANDARD": '10000.00',  # No PEN
+    }
+    
+    income_limits_by_size = {
+        "1": '100100.00',
+        "2": '114380.00',
+        "3": '128660.00',
+        "4": '142940.00',
+        "5": '154420.00',
+    }
+    
+    counties = ["Vine City"]  
+
+    # check for existing program in db
+    existing_program = Program.objects.filter(name=program_name).first()
+    if existing_program:
+        print(f"The program '{program_name}' already exists in the database.")
+        return
+
+    # create and save new program instance
+    vcri_program = Program(
+        name=program_name,
+        amount=down_payment_options,
+        counties=', '.join(counties),
+        income_limits=income_limits_by_size,
+        liquid_asset_limit='25000.00', 
+        credit_score_minimum=0  
+    )
+
+    vcri_program.save()
+    print(f"The program '{program_name}' has been added to the database.")
+
+
 add_georgia_dream_homebuyers_program()
+add_fulton_homeownership_program()
+add_vine_city_renaissance_initiative()
+
+
--- DownPaymentBackend/DPA_app/views.py
+++ DownPaymentBackend/DPA_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import UserInformationForm
+from .models import User, Program
 
-# Create your views here.
+def check_eligibility(request):
+    if request.method == 'POST':
+        form = UserInformationForm(request.POST)
+        if form.is_valid():
+            temp_user = User(**form.cleaned_data)
+            eligible_programs = [program for program in Program.objects.all() if program.is_eligible(temp_user)]
+            return render(request, 'eligibility_results.html', {'programs': eligible_programs})
+    else:
+        form = UserInformationForm()
+    
+    return render(request, 'check_eligibility.html', {'form': form})
