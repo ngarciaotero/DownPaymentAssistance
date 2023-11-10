80592d56f2f2b9bb5b66f414f15199b004f15aad
Nisrine Jednane-Kane <nisrinekane@nisrines-laptop.lan>
2023-11-10 00:56:18 -0800

    Add dynamic data fetching

--- DownPayment/DPA_app/views.py
+++ DownPayment/DPA_app/views.py
@@ -3,13 +3,21 @@ from .forms import UserInformationForm
 from .models import User, Program
 
 def check_eligibility(request):
+    # get unique counties from all programs
+    programs = Program.objects.all()
+    counties = set()
+    for program in programs:
+        program_counties = program.counties.split(', ')
+        counties.update(program_counties)
+
     if request.method == 'POST':
         form = UserInformationForm(request.POST)
         if form.is_valid():
             temp_user = User(**form.cleaned_data)
-            eligible_programs = [program for program in Program.objects.all() if program.is_eligible(temp_user)]
+            eligible_programs = [program for program in programs if program.is_eligible(temp_user)]
             return render(request, 'eligibility_results.html', {'programs': eligible_programs})
     else:
         form = UserInformationForm()
     
-    return render(request, 'check_eligibility.html', {'form': form})
+    return render(request, 'check_eligibility.html', {'form': form, 'counties': counties})
+
